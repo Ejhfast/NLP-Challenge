1fa5a5def7b1815f2c171533a0d3d455ae5c1f8f
Ethan Fast <ethan@ethan-fasts-macbook-pro.local>
2010-11-13 22:14:39 -0500

    faster version

--- process.rb
+++ process.rb
@@ -7,34 +7,36 @@ def parse_vocab vocab, docs
 		pair = line.split
 		word = pair[1]
 		freq = pair[0].to_i
+		puts "start grep"
 		`grep '#{word}' #{docs} > wordfiles/word#{count}`
-		answer = word_freq("wordfiles/word#{count}", word)
-		answer.keys.each do |k|
-			answer[k] = answer[k].to_f / freq.to_f
-		end
-		adjusted_answer = top_ten(answer, word)
-		puts adjusted_answer
-		count = count + 1
+#		puts "start freq"
+#		answer = word_freq("wordfiles/word#{count}", word)
+#		puts "start adjust"
+#		answer.keys.each do |k|
+#			answer[k] = answer[k].to_f / freq.to_f
+#		end
+#		puts "start sort"
+#		adjusted_answer = top_ten(answer, word)
+#		puts adjusted_answer
+#		count = count + 1
 	}
+	f.close
 end
 
 
 def word_freq the_file, excluded
 	# get word frequencies from file, excluding the original word 
-	h = Hash.new
+	h = Hash.new(0)
 	f = File.open(the_file, "r")
 	f.each_line { |line|
   	words = line.split
   	words.each { |w|
 			if w != excluded 
-				if h.has_key?(w)
 					h[w] = h[w] + 1
-				else
-					h[w] = 1
-				end
 			end
 		}
 	}
+	f.close
 	return h
 end
 
