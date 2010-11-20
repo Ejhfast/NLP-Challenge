fc5f1b7edc2386cf078ec2909bbea35397f4e8cf
Ethan Fast <ethan@ethan-fasts-macbook-pro.local>
2010-11-20 06:57:30 -0500

    update

--- process.rb
+++ process.rb
@@ -1,52 +1,55 @@
 # takes two arguments, the name of the vocab file and the name of the document file
 
-def parse_vocab vocab, docs
+def parse_vocab vocab, docs, out
 	count = 0
+	fh = Hash.new
 	f = File.open(vocab, "r")
 	f.each_line{ |line|
-		pair = line.split
+		pair = line.split.map{|x| x.chomp}
 		word = pair[1]
-		freq = pair[0].to_i
-		puts "start grep"
-		`grep '#{word}' #{docs} > wordfiles/word#{count}`
-#		puts "start freq"
-#		answer = word_freq("wordfiles/word#{count}", word)
-#		puts "start adjust"
-#		answer.keys.each do |k|
-#			answer[k] = answer[k].to_f / freq.to_f
-#		end
-#		puts "start sort"
-#		adjusted_answer = top_ten(answer, word)
-#		puts adjusted_answer
-#		count = count + 1
+		freq = 1.0 / pair[0].to_f
+		fh[word] = freq
 	}
 	f.close
+	ans = word_freq docs, fh
+	fo = File.open(out,"w")
+	ans.keys.each do |word|
+		tmp = ans[word].keys.sort_by{|x| -1 * ans[word][x]}.select{|x| x != word}
+		if tmp.size >= 10
+			fo.puts word + " " + tmp.first(10).join(" ")
+		else
+			# look through more tenuous connections
+			rest = 10 - tmp.size
+			fo.puts word + " " + tmp.join(" ") + " " + tmp.first(2).map{|x| ans[x].keys.sort_by{|y| -1 * ans[x][y]}}.flatten.uniq.select{|x| not tmp.include?(x)}.first(rest).join(" ")
+		end	
+	end
+	fo.close
 end
 
 
-def word_freq the_file, excluded
+def word_freq the_file, freq_hash
 	# get word frequencies from file, excluding the original word 
-	h = Hash.new(0)
+	h = Hash.new(nil)
+	count = 0
 	f = File.open(the_file, "r")
 	f.each_line { |line|
-  	words = line.split
+  	puts count
+		words = line.split.map{|x| x.chomp}
   	words.each { |w|
-			if w != excluded 
-					h[w] = h[w] + 1
-			end
+			words.each {|w2|
+				if not h[w].nil?
+					h[w][w2] = h[w][w2] + (1.0 * freq_hash[w2].to_f)
+				else
+					h[w] = Hash.new(0.0)
+					h[w][w2] = (1.0 * freq_hash[w2].to_f)
+				end
+			}
 		}
+	count = count + 1
 	}
 	f.close
 	return h
 end
 
-# sort the hash by value, and then print it in this sorted order
-def top_ten h, word
-	str = word
-	h.sort{|a,b| a[1]<=>b[1]}.last(10).reverse.each { |elem|
-  	str = str + " #{elem[0]}"
-	}
-	return str
-end
 
-parse_vocab ARGV[0], ARGV[1]
+parse_vocab ARGV[0], ARGV[1], ARGV[2]
